72339570636ce1d875fa39f0352b37c0c86c5794
项晨晓 <3270660346@qq.com>
2024-10-19 13:50:55 +0800

    [add] - 条件渲染

--- react/react-vite-demo/src/App.tsx
+++ react/react-vite-demo/src/App.tsx
@@ -1,9 +1,13 @@
 import './App.css'
+import ConditionalRendering from './components/conditionalRendering';
 import DynamicAttribute from './components/dynamicAttribute';
 import MyFirst from './components/myFirst';/* 默认导入 */
 import { MyFirst2 } from './components/myFirst';/* 具名导入 */
+import PassData from './components/passData';
+import PassData2 from './components/passData2';
 
 function App() {
+  const params = {name: 'xcx', age: "22"}
   return (
     <>{/* 5.react必须有一个根组件, 且可以为空(Fragment), 确保不会添加额外的子节点 */}
       {/* <MyFirst></MyFirst> */}
@@ -12,7 +16,14 @@ function App() {
       {/* 4.普通html开头字母小写 */}
       {/* <MyFirst /> */}
       {/* 组件开头字母大写 */}
-      <DynamicAttribute />
+      {/* <DynamicAttribute /> */}
+      {/*  9.传递数据 -- 注意不能直接修改props传递的数据*/}
+      {/* <PassData para/> */}
+      {/* <PassData receive={'数据'} /> */}
+      {/* 10.展开语法中传递数据 */}
+      {/* <PassData2 {...params}></PassData2> */}
+      {/* 11. 条件渲染*/}
+      <ConditionalRendering age={10} today={7} hasMsg={ 'yes' } />
     </>
   )
 }
---
+++ react/react-vite-demo/src/components/conditionalRendering.tsx
@@ -0,0 +1,35 @@
+export default function ConditionalRendering(
+    { age, today, hasMsg }:
+    {
+      age: number, today: number, hasMsg: string
+    })
+{
+  // 11.1使用变量存储执行条件的结果
+  const conValue = age > 18 ?
+  (
+    <span style={{color: 'green'}}>成年</span>
+  ) :
+  (
+    <span style={{color: 'red'}}>未成年</span>
+  ) 
+  return (
+    <>
+      {conValue}
+      <br />
+      {/* 11.2 直接使用条件表达式 */}
+      {
+        today === 7 ? 
+          (
+            <span>放假</span>
+          ) :
+          (
+            <span>上班</span>
+          )
+      }
+      {/* 11.3 使用&简化条件 */}
+      {hasMsg && (
+        <span>{ hasMsg }</span>
+      )}
+    </>
+  )
+}
