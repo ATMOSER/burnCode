b835954a6ab43e3c37377984939132329518379d
项晨晓 <3270660346@qq.com>
2024-10-19 10:41:05 +0800

    [add] - 传递动态属性

--- react/react-vite-demo/src/components/dynamicAttribute.tsx
+++ react/react-vite-demo/src/components/dynamicAttribute.tsx
@@ -1,10 +1,18 @@
 export default function DynamicAttribute() {
   const url = 'https://i.imgur.com/7vQD0fPs.jpg'
+  const age = 10
   return (
     <>
-      {/* 使用动态属性 */}
+      {/* 6.使用动态属性 */}
       <img src={ url } alt="是个男的" className="human" />
       {/* <img src="https://i.imgur.com/7vQD0fPs.jpg" alt="是个男的"/> */}
+      {/* 7.运行表达式 */}
+      <span>{age > 18 ? "成年" : "未成年"}</span>
+      {/* 8.编写样式 */}
+      <span style={
+        { color: 'red' }
+      // 只是大括号语法中的对象
+      }>red</span>
     </>
   )
 }
